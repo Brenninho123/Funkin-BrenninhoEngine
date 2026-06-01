98e46af7b6883c96f1c2f2704b68dc3654ab53ca
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-01 19:28:20 -0300

    Atualizar o MobileLuaBridge.hx

--- source/mobile/psychlua/MobileLuaBridge.hx
+++ source/mobile/psychlua/MobileLuaBridge.hx
@@ -1,20 +1,13 @@
 package mobile.psychlua;
 
-#if (mobile && LUA_ALLOWED)
-import mobile.psychlua.MobileFunctions;
-#if android
-import mobile.psychlua.AndroidFunctions;
-#end
-#end
-
 class MobileLuaBridge
 {
 	public static function implement(funk:psychlua.FunkinLua):Void
 	{
 		#if (mobile && LUA_ALLOWED)
-		MobileFunctions.implement(funk);
+		mobile.psychlua.MobileFunctions.implement(funk);
 		#if android
-		AndroidFunctions.implement(funk);
+		mobile.psychlua.AndroidFunctions.implement(funk);
 		#end
 		#end
 	}
