9e98616da8c4fed0e6c5db8460fde063e8647104
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-02 15:38:45 -0300

    Atualizar o MasterEditorMenu.hx

--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -10,6 +10,7 @@ class MasterEditorMenu extends MusicBeatState
 	static final OPTIONS:Array<String> = [
 		'Chart Editor',
 		'Character Editor',
+		'Stage Editor',
 		'Week Editor',
 		'Menu Character Editor',
 		'Dialogue Editor',
@@ -19,7 +20,7 @@ class MasterEditorMenu extends MusicBeatState
 
 	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var directories:Array<String> = [null];
-	private var curSelected:Int = 0;
+	private var curSelected:Int  = 0;
 	private var curDirectory:Int = 0;
 	private var directoryTxt:FlxText;
 
@@ -28,7 +29,7 @@ class MasterEditorMenu extends MusicBeatState
 		FlxG.camera.bgColor = FlxColor.BLACK;
 
 		#if DISCORD_ALLOWED
-		DiscordClient.changePresence("Editors Main Menu", null);
+		DiscordClient.changePresence('Editors Main Menu', null);
 		#end
 
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
@@ -43,7 +44,7 @@ class MasterEditorMenu extends MusicBeatState
 		{
 			var leText:Alphabet = new Alphabet(90, 320, OPTIONS[i], true);
 			leText.isMenuItem = true;
-			leText.targetY = i;
+			leText.targetY    = i;
 			leText.snapToPosition();
 			grpTexts.add(leText);
 		}
@@ -84,18 +85,12 @@ class MasterEditorMenu extends MusicBeatState
 
 	override function update(elapsed:Float):Void
 	{
-		if (controls.UI_UP_P)
-			changeSelection(-1);
-
-		if (controls.UI_DOWN_P)
-			changeSelection(1);
+		if (controls.UI_UP_P)   changeSelection(-1);
+		if (controls.UI_DOWN_P) changeSelection(1);
 
 		#if MODS_ALLOWED
-		if (controls.UI_LEFT_P)
-			changeDirectory(-1);
-
-		if (controls.UI_RIGHT_P)
-			changeDirectory(1);
+		if (controls.UI_LEFT_P)  changeDirectory(-1);
+		if (controls.UI_RIGHT_P) changeDirectory(1);
 		#end
 
 		if (controls.BACK)
@@ -112,6 +107,8 @@ class MasterEditorMenu extends MusicBeatState
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
 				case 'Character Editor':
 					LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
+				case 'Stage Editor':
+					MusicBeatState.switchState(new StageEditorState());
 				case 'Week Editor':
 					MusicBeatState.switchState(new WeekEditorState());
 				case 'Menu Character Editor':
@@ -129,7 +126,7 @@ class MasterEditorMenu extends MusicBeatState
 		for (item in grpTexts.members)
 		{
 			item.targetY = index - curSelected;
-			item.alpha = (item.targetY == 0) ? 1.0 : 0.6;
+			item.alpha   = (item.targetY == 0) ? 1.0 : 0.6;
 			index++;
 		}
 
@@ -152,9 +149,7 @@ class MasterEditorMenu extends MusicBeatState
 
 		var dir:String = directories[curDirectory];
 		if (dir == null || dir.length < 1)
-		{
 			directoryTxt.text = '< NO MOD DIRECTORY LOADED >';
-		}
 		else
 		{
 			Mods.currentModDirectory = dir;
