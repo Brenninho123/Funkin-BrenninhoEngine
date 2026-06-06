8863816a636bd49cc066b3af75cc3b6d5ce94552
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-06 12:37:39 -0300

    Atualizar o StorageUtil.hx

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -12,11 +12,16 @@ class StorageUtil
 	public static function getStorageDirectory(?force:Bool = false):String
 	{
 		#if android
-		if (!FileSystem.exists(rootDir + 'storagetype.txt'))
-			File.saveContent(rootDir + 'storagetype.txt', ClientPrefs.data.storageType);
+		var typeFile:String = rootDir + 'storagetype.txt';
 
-		var curStorageType:String = File.getContent(rootDir + 'storagetype.txt');
-		var daPath:String = force ? StorageType.fromStrForce(curStorageType) : StorageType.fromStr(curStorageType);
+		if (!FileSystem.exists(typeFile))
+			File.saveContent(typeFile, ClientPrefs.data.storageType);
+
+		var curStorageType:String = File.getContent(typeFile);
+		var daPath:String         = force
+			? StorageType.fromStrForce(curStorageType)
+			: StorageType.fromStr(curStorageType);
+
 		return Path.addTrailingSlash(daPath);
 		#elseif ios
 		return LimeSystem.documentsDirectory;
@@ -44,10 +49,44 @@ class StorageUtil
 		}
 	}
 
+	public static function fileExists(path:String):Bool
+	{
+		#if sys
+		return FileSystem.exists(path);
+		#else
+		return false;
+		#end
+	}
+
+	public static function ensureDirectory(path:String):Bool
+	{
+		try
+		{
+			if (!FileSystem.exists(path))
+				FileSystem.createDirectory(path);
+			return true;
+		}
+		catch (e:Dynamic) { return false; }
+	}
+
+	public static function readContent(path:String):Null<String>
+	{
+		try { return File.getContent(path); }
+		catch (e:Dynamic) { return null; }
+	}
+
+	public static function writeContent(path:String, data:String):Bool
+	{
+		try { File.saveContent(path, data); return true; }
+		catch (e:Dynamic) { return false; }
+	}
+
 	#if android
 	public static function requestPermissions():Void
 	{
-		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
+		var isTiramisu:Bool = AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU;
+
+		if (isTiramisu)
 			AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO']);
 		else
 			AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
@@ -59,25 +98,25 @@ class StorageUtil
 			AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
 		}
 
-		final isTimaraisu:Bool = AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU;
-		final grantedPerms:Array<String> = AndroidPermissions.getGrantedPermissions();
-		final missingPerm:Bool = isTimaraisu
+		var grantedPerms:Array<String> = AndroidPermissions.getGrantedPermissions();
+		var missingPerm:Bool = isTiramisu
 			? !grantedPerms.contains('android.permission.READ_MEDIA_IMAGES')
 			: !grantedPerms.contains('android.permission.READ_EXTERNAL_STORAGE');
 
 		if (missingPerm)
-			CoolUtil.showPopUp('If you accepted the permissions you are all good!\nIf you didn\'t then expect a crash\nPress OK to see what happens', 'Notice!');
+			CoolUtil.showPopUp(
+				'If you accepted the permissions you are all good!\nIf you didn\'t then expect a crash\nPress OK to see what happens',
+				'Notice!'
+			);
 
-		final storageDir:String = StorageUtil.getStorageDirectory();
+		var storageDir:String = getStorageDirectory();
 
-		try
+		if (!ensureDirectory(storageDir))
 		{
-			if (!FileSystem.exists(storageDir))
-				FileSystem.createDirectory(storageDir);
-		}
-		catch (e:Dynamic)
-		{
-			CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getStorageDirectory(true) + '\nPress OK to close the game', 'Error!');
+			CoolUtil.showPopUp(
+				'Please create directory to\n' + getStorageDirectory(true) + '\nPress OK to close the game',
+				'Error!'
+			);
 			LimeSystem.exit(1);
 		}
 	}
@@ -90,7 +129,8 @@ class StorageUtil
 		if (splitStorage)
 			paths = paths.replace('/storage/', '');
 
-		return paths.split(',');
+		var result:Array<String> = paths.split(',');
+		return result.filter((p:String) -> p.length > 0);
 	}
 
 	public static function getExternalDirectory(externalDir:String):String
@@ -106,6 +146,42 @@ class StorageUtil
 
 		return Path.addTrailingSlash(daPath);
 	}
+
+	public static function getTotalStorageGB():Float
+	{
+		try
+		{
+			var stat = new sys.io.Process('df -BG ' + getStorageDirectory());
+			var output:String = stat.stdout.readAll().toString();
+			var lines:Array<String> = output.split('\n');
+			if (lines.length > 1)
+			{
+				var parts:Array<String> = lines[1].split(' ').filter((s:String) -> s.length > 0);
+				if (parts.length > 1)
+					return Std.parseFloat(parts[1].replace('G', ''));
+			}
+		}
+		catch (e:Dynamic) {}
+		return 0.0;
+	}
+
+	public static function getAvailableStorageGB():Float
+	{
+		try
+		{
+			var stat = new sys.io.Process('df -BG ' + getStorageDirectory());
+			var output:String = stat.stdout.readAll().toString();
+			var lines:Array<String> = output.split('\n');
+			if (lines.length > 1)
+			{
+				var parts:Array<String> = lines[1].split(' ').filter((s:String) -> s.length > 0);
+				if (parts.length > 3)
+					return Std.parseFloat(parts[3].replace('G', ''));
+			}
+		}
+		catch (e:Dynamic) {}
+		return 0.0;
+	}
 	#end
 	#end
 }
@@ -114,28 +190,28 @@ class StorageUtil
 @:runtimeValue
 enum abstract StorageType(String) from String to String
 {
-	static final forcedPath:String = '/storage/emulated/0/';
+	static final forcedPath:String      = '/storage/emulated/0/';
 	static final packageNameLocal:String = 'com.funkin.brenninhoengine';
-	static final fileLocal:String = 'BrenninhoEngine';
+	static final fileLocal:String        = 'BrenninhoEngine';
 
-	var EXTERNAL_DATA = "EXTERNAL_DATA";
-	var EXTERNAL_OBB = "EXTERNAL_OBB";
+	var EXTERNAL_DATA  = "EXTERNAL_DATA";
+	var EXTERNAL_OBB   = "EXTERNAL_OBB";
 	var EXTERNAL_MEDIA = "EXTERNAL_MEDIA";
-	var EXTERNAL = "EXTERNAL";
+	var EXTERNAL       = "EXTERNAL";
 
 	public static function fromStr(str:String):StorageType
 	{
-		final pkgName:String = lime.app.Application.current.meta.get('packageName');
-		final fileName:String = lime.app.Application.current.meta.get('file');
-		final extStorage:String = AndroidEnvironment.getExternalStorageDirectory();
+		var pkgName:String    = lime.app.Application.current.meta.get('packageName');
+		var fileName:String   = lime.app.Application.current.meta.get('file');
+		var extStorage:String = AndroidEnvironment.getExternalStorageDirectory();
 
 		return switch (str)
 		{
-			case "EXTERNAL_DATA": AndroidContext.getExternalFilesDir();
-			case "EXTERNAL_OBB": AndroidContext.getObbDir();
+			case "EXTERNAL_DATA":  AndroidContext.getExternalFilesDir();
+			case "EXTERNAL_OBB":   AndroidContext.getObbDir();
 			case "EXTERNAL_MEDIA": '$extStorage/Android/media/$pkgName';
-			case "EXTERNAL": '$extStorage/.$fileName';
-			default: StorageUtil.getExternalDirectory(str) + '.$fileLocal';
+			case "EXTERNAL":       '$extStorage/.$fileName';
+			default:               StorageUtil.getExternalDirectory(str) + '.$fileLocal';
 		}
 	}
 
@@ -143,11 +219,28 @@ enum abstract StorageType(String) from String to String
 	{
 		return switch (str)
 		{
-			case "EXTERNAL_DATA": '${forcedPath}Android/data/$packageNameLocal/files';
-			case "EXTERNAL_OBB": '${forcedPath}Android/obb/$packageNameLocal';
+			case "EXTERNAL_DATA":  '${forcedPath}Android/data/$packageNameLocal/files';
+			case "EXTERNAL_OBB":   '${forcedPath}Android/obb/$packageNameLocal';
 			case "EXTERNAL_MEDIA": '${forcedPath}Android/media/$packageNameLocal';
-			case "EXTERNAL": '$forcedPath.$fileLocal';
-			default: StorageUtil.getExternalDirectory(str) + '.$fileLocal';
+			case "EXTERNAL":       '$forcedPath.$fileLocal';
+			default:               StorageUtil.getExternalDirectory(str) + '.$fileLocal';
+		}
+	}
+
+	public static function getAll():Array<StorageType>
+	{
+		return [EXTERNAL_DATA, EXTERNAL_OBB, EXTERNAL_MEDIA, EXTERNAL];
+	}
+
+	public static function getDisplayName(type:StorageType):String
+	{
+		return switch (type)
+		{
+			case "EXTERNAL_DATA":  'External Data';
+			case "EXTERNAL_OBB":   'External OBB';
+			case "EXTERNAL_MEDIA": 'External Media';
+			case "EXTERNAL":       'External';
+			default:               'Custom ($type)';
 		}
 	}
 }
