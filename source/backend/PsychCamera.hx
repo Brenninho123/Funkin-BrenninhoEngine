88085be55437a2c9c90e008ad89fe83da14503b6
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-06-13 17:26:14 -0300

    Update Ass Shit

--- source/backend/PsychCamera.hx
+++ source/backend/PsychCamera.hx
@@ -1,17 +1,11 @@
 package backend;
 
-// PsychCamera handles followLerp based on elapsed
-// and stops camera from snapping at higher framerates
-
 class PsychCamera extends FlxCamera
 {
 	override public function update(elapsed:Float):Void
 	{
-		// follow the target, if there is one
 		if (target != null)
-		{
 			updateFollowDelta(elapsed);
-		}
 
 		updateScroll();
 		updateFlash(elapsed);
@@ -25,8 +19,6 @@ class PsychCamera extends FlxCamera
 
 	public function updateFollowDelta(?elapsed:Float = 0):Void
 	{
-		// Either follow the object closely,
-		// or double check our deadzone and update accordingly.
 		if (deadzone == null)
 		{
 			target.getMidpoint(_point);
@@ -42,57 +34,37 @@ class PsychCamera extends FlxCamera
 			if (style == SCREEN_BY_SCREEN)
 			{
 				if (targetX >= viewRight)
-				{
 					_scrollTarget.x += viewWidth;
-				}
 				else if (targetX + target.width < viewLeft)
-				{
 					_scrollTarget.x -= viewWidth;
-				}
 
 				if (targetY >= viewBottom)
-				{
 					_scrollTarget.y += viewHeight;
-				}
 				else if (targetY + target.height < viewTop)
-				{
 					_scrollTarget.y -= viewHeight;
-				}
-				
-				// without this we see weird behavior when switching to SCREEN_BY_SCREEN at arbitrary scroll positions
+
 				bindScrollPos(_scrollTarget);
 			}
 			else
 			{
 				edge = targetX - deadzone.x;
-				if (_scrollTarget.x > edge)
-				{
-					_scrollTarget.x = edge;
-				}
+				if (_scrollTarget.x > edge) _scrollTarget.x = edge;
+
 				edge = targetX + target.width - deadzone.x - deadzone.width;
-				if (_scrollTarget.x < edge)
-				{
-					_scrollTarget.x = edge;
-				}
+				if (_scrollTarget.x < edge) _scrollTarget.x = edge;
 
 				edge = targetY - deadzone.y;
-				if (_scrollTarget.y > edge)
-				{
-					_scrollTarget.y = edge;
-				}
+				if (_scrollTarget.y > edge) _scrollTarget.y = edge;
+
 				edge = targetY + target.height - deadzone.y - deadzone.height;
-				if (_scrollTarget.y < edge)
-				{
-					_scrollTarget.y = edge;
-				}
+				if (_scrollTarget.y < edge) _scrollTarget.y = edge;
 			}
 
 			if ((target is FlxSprite))
 			{
 				if (_lastTargetPosition == null)
-				{
-					_lastTargetPosition = FlxPoint.get(target.x, target.y); // Creates this point.
-				}
+					_lastTargetPosition = FlxPoint.get(target.x, target.y);
+
 				_scrollTarget.x += (target.x - _lastTargetPosition.x) * followLead.x;
 				_scrollTarget.y += (target.y - _lastTargetPosition.y) * followLead.y;
 
@@ -104,10 +76,11 @@ class PsychCamera extends FlxCamera
 		var mult:Float = 1 - Math.exp(-elapsed * followLerp);
 		scroll.x += (_scrollTarget.x - scroll.x) * mult;
 		scroll.y += (_scrollTarget.y - scroll.y) * mult;
-		//trace('lerp on this frame: $mult');
 	}
 
-	override function set_followLerp(value:Float)
+	public var followLerp(default, set):Float = 0;
+
+	function set_followLerp(value:Float):Float
 	{
 		return followLerp = value;
 	}
